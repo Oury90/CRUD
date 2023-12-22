b1362356f0fd74c3ced39072e5f634c9fab7cb71
Diallo Amadou Oury <dialloamadououry@MacBook-Pro-de-Diallo.local>
2023-12-22 05:32:15 +0100

    The correct crud

--- index.js
+++ index.js
@@ -2,51 +2,70 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
+
 const app = express();
-const server_url = "http://localhost:3000";
-const port = 4000;
+const URL = "http://localhost:4000";
+const port = 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
 app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 
 
 app.get("/", async(req, res) =>{
-    const response = await axios.get(server_url);
-    const data = response.data;
-    console.log(data);
-    res.render("index.ejs", {
-        dataJoke: data
-    });
-})
-
-app.get("/form", (req, res) =>{
-    res.render("form.ejs");
-})
-
-// post a new joke
-app.post("/post", async(req, res) =>{
-
     try {
-        const response = await axios.post(`${server_url}/posts`, req.body);
+        const response = await axios.get(`${URL}`);
         const data = response.data;
-        console.log(data);
+        res.render("index.ejs",{
+            posts:data
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get("/new", (req, res) =>{
+    res.render("form.ejs");
+});
+
+app.get("/edit/:id", async(req, res) =>{
+    try {
+        const response = await axios.get(`${URL}/posts/${req.params.id}`);
+        const data = response.data;
+        res.render("form.ejs", {
+            post: data
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.post("/api/posts", async(req, res) =>{
+    try {
+        const response = await axios.post(`${URL}/posts`, req.body);
         res.redirect("/");
     } catch (error) {
-        console.error();
+        console.log(error);
     }
-   
 })
-// edit post
 
-app.get("/posts/:id", async(req, rs) =>{
-    const response = await axios.get(`${server_url}/posts/${req.params.id}`);
-    const data  = response.data;
-    console.log(data.joke);
-    
+app.post("/api/posts/:id", async(req, res) =>{
+    try {
+        const response = await axios.patch(`${URL}/posts/${req.params.id}`, req.body);
+        console.log(response.data);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get("/api/delete/:id", async(req, res) =>{
+    await axios.delete(`${URL}/posts/${req.params.id}`);
+    res.redirect("/");
 })
 
 app.listen(port, () =>{
-    console.log(`This server is running on port ${port}`);
+    console.log(`This server is ruinning on port ${port}`);
 })
--- server.js
+++ server.js
@@ -1,70 +1,107 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-let numJokes = 3;
+let numId = 3;
 
-const jokes = [
-    {
-        "id": 1,
-      "joke": "I just got my doctor's test results and I’m really upset. Turns out, I’m not gonna be a doctor.",
-      "author": "Diallo Amadou Ourys"
-    },
-    {
-        "id": 2,
-      "joke": "I have a joke about chemistry, but I don’t think it’ll get a reaction.",
-      "author": "Bah"
-    },
-    {
-        "id": 3,
-      "joke": "If a child refuses to nap, are they guilty of resisting a rest?",
-      "author": "Barry"
-    }
-  ];
+// const data = new Date().toLocaleTimeString();
 
-// get a random joke
+
+let posts = [
+  // {
+  //   "id":1,
+  //   "quote": "Derive happiness in oneself from a good day's work, from illuminating the fog that surrounds us.",
+  //   "author": "Henri Matisse",
+  //   "category": "happiness"
+  // },
+  // {
+  //   "id": 2,
+  //   "quote": "There can be no happiness if the things we believe in are different from the things we do.",
+  //   "author": "Freya Stark",
+  //   "category": "happiness"
+  // },
+  // {
+  //   "id": 3,
+  //   "quote": "Man's only true happiness is to live in hope of something to be won by him. Reverence something to be worshipped by him, and love something to be cherished by him, forever.",
+  //   "author": "John Ruskin",
+  //   "category": "happiness"
+  // }
+
+  {
+    id:1,
+    quote: "Derive happiness in oneself from a good day's work, from illuminating the fog that surrounds us.",
+    author: "Henri Matisse",
+    category: "happiness"
+  },
+  {
+    id: 2,
+    quote: "There can be no happiness if the things we believe in are different from the things we do.",
+    author: "Freya Stark",
+    category: "happiness"
+  },
+  {
+    id: 3,
+    quote: "Man's only true happiness is to live in hope of something to be won by him. Reverence something to be worshipped by him, and love something to be cherished by him, forever.",
+    author: "John Ruskin",
+    category: "happiness"
+  }
+];
+
+// get all posts
 app.get("/", (req, res) =>{
-    const randomJokes = Math.floor(Math.random() * jokes.length);
-    res.json(jokes[randomJokes]);
+  res.json(posts);
 })
-// post some joke in the list jokes
-app.post("/posts", (req, res) =>{
-    const newId = numJokes +1;
-    const newJoke = {
-        id: newId,
-        joke: req.body.joke,
-        author: req.body.author,
-    }
-    numJokes = newId;
-    jokes.push(newJoke);
-    res.json(newJoke);
 
+// get a specifique post
+app.get("/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+  res.json(post);
+});
+// new posts
+
+app.post("/posts", (req, res) =>{
+  const newId = numId +1;
+  const newPost = {
+    id: newId,
+    quote: req.body.quote,
+    author: req.body.author,
+    category: req.body.category
+  }
+  numId= newPost;
+  posts.push(newPost);
+  res.json(newPost);
 })
-// Edit post
+// Edit the post
 app.patch("/posts/:id", (req, res) =>{
-    const id = parseInt(req.params.id);
-    const editJoke = jokes.find((joke) => joke.id === id);
-    if(!editJoke) return res.status(400).json({message: "Joke not found"});
-    if(req.body.joke) editJoke.joke = req.body.joke;
-    if(req.body.author) editJoke.author = req.body.author;
-    res.json(editJoke);
+  const id = parseInt(req.params.id);
+  const postEdit = posts.find((post) => post.id ===id);
+  if(!postEdit) return res.status(400).json({message: "Post not found"});
+
+  if(req.body.quote) postEdit.quote = req.body.quote;
+  if(req.body.author) postEdit.author = req.body.author;
+  if(req.body.category) postEdit.category = req.body.category;
+  res.json(postEdit);
 })
-// Delete joke
+
+// delete post
 app.delete("/posts/:id", (req, res) =>{
-    const id = parseInt(req.params.id);
-    const index = jokes.findIndex((joke) => joke.id === id);
-    if(index === -1) return res.status(400).json({message: "Joke not deleted"});
-    jokes.splice(index, 1);
-    res.json("message deleted")
+  const id = parseInt(req.params.id);
+  const index = posts.findIndex((post) => post.id = id);
+  if(index === -1) return res.status(500).json({message: "Post not deleted"});
+  posts.splice(index, 1);
+  res.json({message: "Post deleted"});
 })
+
+
+
 
 
 app.listen(port, () =>{
-    console.log(`This server is running on port http://localhost:${port}`);
+  console.log(`This server is running on port http://localhost:${port}`);
 })
